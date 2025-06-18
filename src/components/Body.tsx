0c206b803139250dff4ac91cd22094f7184387cb
n4ryn <vinaykumar.cs19@aryacollege.in>
2025-06-18 23:56:14 +0530

    feat: update header and footer

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,5 +1,9 @@
 const Body = () => {
-  return <div>Body</div>;
+  return (
+    <div className="p-4 w-full min-h-[calc(100vh-140px)] flex flex-col justify-center items-center">
+      Body
+    </div>
+  );
 };
 
 export default Body;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,16 @@
+import { RxPlus } from "react-icons/rx";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex justify-between items-center p-4">
+      <p className="font-semibold">
+        Ai<span className="text-blue-400">Speech</span>
+      </p>
+      <button className="bg-white text-blue-400 p-2 rounded-lg shadow-2xl shadow-blue-400/60 flex justify-center items-center gap-2 cursor-pointer">
+        New <RxPlus />
+      </button>
+    </div>
+  );
 };
 
 export default Header;
